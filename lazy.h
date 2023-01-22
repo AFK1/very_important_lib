17eeb55ddc96753508a9409e7bdb491cf74c1898
sergwest <ksendzov.sd@gmail.com>
2023-01-22 23:43:25 +0300

    lazy update

---
+++ lazy.h
@@ -0,0 +1,8 @@
+typedef int i32
+typedef long i32 i64
+typedef short i32 i16
+typedef short i16 i8
+typedef unsigned i64 u64
+typedef unsigned i32 u32
+typedef unsigned i16 u16
+typedef unsigned i8 u8
